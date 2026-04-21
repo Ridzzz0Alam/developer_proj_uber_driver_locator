2a74615d8a10ca61e269a605d79c8a6de8c265c5
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-04-21 12:19:28 +0200

    added redis key for all drivers

--- location-service/src/main/java/com/rideshare/locationservice/service/LocationService.java
+++ location-service/src/main/java/com/rideshare/locationservice/service/LocationService.java
@@ -1,7 +1,23 @@
 package com.rideshare.locationservice.service;
 
+import com.rideshare.locationservice.dto.DriverLocationRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class LocationService {
+
+    //Redis key for all driver locations
+    private static final String DRIVERS_GEO_KEY = "drivers:locations";
+
+    /**
+     * Update driver location in Redis
+     * And calling every 3 seconds by drivers phone
+     * And Map to Redis GEOADD command
+     */
+
+    public void updateDriverLocation(DriverLocationRequest driverLocationRequest){
+        log.info("Updating location for driver: {}", driverLocationRequest.getDriverId());
+    }
 }
