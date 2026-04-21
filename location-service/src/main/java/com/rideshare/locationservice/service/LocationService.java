75f554e86678adc9950099dfc5e9347c87534f6a
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-04-21 12:24:36 +0200

    added geo latitude and longitude

--- location-service/src/main/java/com/rideshare/locationservice/service/LocationService.java
+++ location-service/src/main/java/com/rideshare/locationservice/service/LocationService.java
@@ -1,13 +1,18 @@
 package com.rideshare.locationservice.service;
 
 import com.rideshare.locationservice.dto.DriverLocationRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class LocationService {
 
+    private final RedisTemplate<String, String> redisTemplate;
     //Redis key for all driver locations
     private static final String DRIVERS_GEO_KEY = "drivers:locations";
 
@@ -19,5 +24,11 @@ public class LocationService {
 
     public void updateDriverLocation(DriverLocationRequest driverLocationRequest){
         log.info("Updating location for driver: {}", driverLocationRequest.getDriverId());
+
+        // IMP NOTE - longitude is always FIRST, and latitude is SECOND, since GeoSpatial standard
+        Point driverPoint = new Point(
+                driverLocationRequest.getLongitude(),
+                driverLocationRequest.getLatitude()
+        );
     }
 }
