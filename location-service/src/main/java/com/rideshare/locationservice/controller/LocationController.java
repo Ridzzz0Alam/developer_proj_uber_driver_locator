a55bffdc53754e2dacaf65c58680315d9467000e
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-04-21 12:09:02 +0200

    Initial commit

---
+++ location-service/src/main/java/com/rideshare/locationservice/controller/LocationController.java
@@ -0,0 +1,44 @@
+package com.rideshare.locationservice.controller;
+
+import com.rideshare.locationservice.dto.DriverLocationRequest;
+import com.rideshare.locationservice.dto.NearByDriverResponse;
+import com.rideshare.locationservice.service.LocationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/locations")
+@Slf4j
+@RequiredArgsConstructor
+public class LocationController {
+
+    private final LocationService locationService;
+
+    //1st Method the drivers phone will call this every 3 seconds
+    @PostMapping("/drivers/update")
+    public ResponseEntity<String> updateDriverLocation(
+            @RequestBody DriverLocationRequest driverLocationRequest){
+        locationService.updateDriverLocation(driverLocationRequest);
+        return ResponseEntity.ok("Driver Location updated");
+    }
+
+    //2nd Method Matching service calls this when ride is requested
+    @GetMapping("/drivers/nearby")
+    public ResponseEntity<List<NearByDriverResponse>> getNearByDrivers(
+            @RequestParam double latitude,
+            @RequestParam double longitude,
+            @RequestParam (defaultValue = "5.0") double radius){
+     return ResponseEntity.ok(locationService.findNearbyDrivers(latitude, longitude, radius));
+    }
+
+    //When driver goes offline
+    @DeleteMapping("/drivers/{driverID}")
+    public ResponseEntity<String> removeDriver(@PathVariable String driverID){
+        locationService.removeDriver(driverID);
+        return ResponseEntity.ok("Driver removed successfully");
+    }
+}
---
+++ location-service/src/main/java/com/rideshare/locationservice/service/LocationService.java
@@ -0,0 +1,7 @@
+package com.rideshare.locationservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LocationService {
+}
