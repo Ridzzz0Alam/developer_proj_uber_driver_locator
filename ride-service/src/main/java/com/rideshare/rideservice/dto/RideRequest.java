0fa094d44b7d2d92ca641aae8932eb3736b5209c
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-04-22 17:40:40 +0200

    started with the package ride request

---
+++ ride-service/src/main/java/com/rideshare/rideservice/dto/RideRequest.java
@@ -0,0 +1,21 @@
+package com.rideshare.rideservice.dto;
+
+
+
+public class RideRequest {
+
+    
+    private String riderID;
+
+    private double pickUpLatitude;
+
+    private double getPickUpLongitude;
+
+    private String pickupAddress;
+
+    private double dropLatitude;
+
+    private double dropLongitude;
+
+    private String dropAddress;
+}
