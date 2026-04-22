67d00b1638b5340551355b2d2484efdb3d77b4e3
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-04-22 16:49:15 +0200

    added ride status enum

--- ride-service/src/main/java/com/rideshare/rideservice/model/Ride.java
+++ ride-service/src/main/java/com/rideshare/rideservice/model/Ride.java
@@ -1,29 +1,46 @@
 package com.rideshare.rideservice.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "rides")
+@Data
+@NoArgsConstructor
+@NoArgsConstructor
 public class Ride {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
+    //Who requested the ride
+    @Column(nullable = false)
     private String riderId;
 
+    //Who accepted the ride (null until matched)
+    @Column(nullable = false)
     private String drvierId;
 
+    @Column(nullable = false)
     private double pickupLatitude;
 
+    @Column(nullable = false)
     private double pickupLongitude;
 
+    @Column(nullable = false)
     private String pickupAddress;
 
+    @Column(nullable = false)
     private double dropLatitude;
 
+    @Column(nullable = false)
     private double dropLongitude;
 
+    @Column(nullable = false)
     private String dropAddress;
 
     //Ride status = tracks the lifecycle
