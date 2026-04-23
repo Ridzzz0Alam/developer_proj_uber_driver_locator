8e6fc3713cdd2fc66b4bc6a01c8b68153f820d29
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-04-23 16:24:32 +0200

    started with ride services

--- ride-service/src/main/java/com/rideshare/rideservice/controller/RideController.java
+++ ride-service/src/main/java/com/rideshare/rideservice/controller/RideController.java
@@ -1,4 +1,29 @@
 package com.rideshare.rideservice.controller;
 
+import com.rideshare.rideservice.dto.RideRequest;
+import com.rideshare.rideservice.dto.RideResponse;
+import com.rideshare.rideservice.service.RideService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/rides")
+@Slf4j
+@RequiredArgsConstructor
 public class RideController {
+
+    private final RideService rideService;
+
+
+    //Rider requests a new ride
+
+    public ResponseEntity<RideResponse> requestRide(
+            @Valid @RequestBody RideRequest rideRequest){
+        
+    }
 }
--- ride-service/src/main/java/com/rideshare/rideservice/service/RideService.java
+++ ride-service/src/main/java/com/rideshare/rideservice/service/RideService.java
@@ -1,4 +1,7 @@
 package com.rideshare.rideservice.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RideService {
 }
