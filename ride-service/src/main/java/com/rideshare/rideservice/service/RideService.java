f5a08fe24f5ec7521cc214056d996e2896dafd9c
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-04-23 22:35:25 +0200

    #completed rider service code

--- ride-service/src/main/java/com/rideshare/rideservice/service/RideService.java
+++ ride-service/src/main/java/com/rideshare/rideservice/service/RideService.java
@@ -1,7 +1,22 @@
 package com.rideshare.rideservice.service;
 
+import com.rideshare.rideservice.event.RideRequestedEvent;
+import com.rideshare.rideservice.repository.RideRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class RideService {
+    private final RideRepository rideRepository;
+    private final KafkaTemplate<String, RideRequestedEvent> kafkaTemplate;
+
+    private static final String RIDE_REQUESTED_TOPIC = "ride.requested";
+
+    /**
+     * 
+     */
 }
