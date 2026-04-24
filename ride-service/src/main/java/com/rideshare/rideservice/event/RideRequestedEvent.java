fc08648d9e3c1d54572ea98daed53008c4d8fc6b
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-04-24 09:12:45 +0200

    added fare estiamtion

--- ride-service/src/main/java/com/rideshare/rideservice/event/RideRequestedEvent.java
+++ ride-service/src/main/java/com/rideshare/rideservice/event/RideRequestedEvent.java
@@ -1,4 +1,22 @@
 package com.rideshare.rideservice.event;
 
+/**
+ * Event published to Kafka when a ride is requested
+ * Matching service consumes this event
+ * TOPIC: ride.requested
+ */
+
 public class RideRequestedEvent {
+    private String rideID;
+    private String riderID;
+
+    //PICKUP
+    private double pickupLatitude;
+    private double pickupLongitude;
+    private String pickupAddress;
+
+    //DROP
+    private double dropLatitude;
+    private double dropLongitude;
+    private String dropAddress;
 }
--- ride-service/src/main/java/com/rideshare/rideservice/service/RideService.java
+++ ride-service/src/main/java/com/rideshare/rideservice/service/RideService.java
@@ -4,6 +4,7 @@ import com.rideshare.rideservice.dto.RideRequest;
 import com.rideshare.rideservice.dto.RideResponse;
 import com.rideshare.rideservice.event.RideRequestedEvent;
 import com.rideshare.rideservice.model.Ride;
+import com.rideshare.rideservice.model.RideStatus;
 import com.rideshare.rideservice.repository.RideRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -29,6 +30,23 @@ public class RideService {
         //Step 1: save ride to database
 
         Ride ride = new Ride();
-        ride.setRiderId();
+        ride.setRiderId(request.getRiderID());
+        ride.setPickupLatitude(request.getPickUpLatitude());
+        ride.setPickupLongitude(request.getGetPickUpLongitude());
+        ride.setPickupAddress(request.getPickupAddress());
+        ride.setDropLatitude(request.getDropLatitude());
+        ride.setDropLongitude(request.getDropLongitude());
+        ride.setDropAddress(request.getDropAddress());
+        ride.setStatus(RideStatus.REQUESTED);
+        ride.setEstimatedFare(calculateEstimateFare(request));
+
+        Ride savedRide = rideRepository.save(ride);
+
+        // Step 2: Publish event to Kafka
+        // Mathcing service will consume and will find nearest driver
+
+        RideRequestedEvent event = new RideRequestedEvent(
+
+        )
     }
 }
