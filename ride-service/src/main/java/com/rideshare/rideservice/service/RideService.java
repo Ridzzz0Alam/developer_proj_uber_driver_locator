be84d218f8c0e3d07758f3abfe1bfbbe88a8f85e
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-04-24 08:56:25 +0200

    started with how user requests ride part

--- ride-service/src/main/java/com/rideshare/rideservice/service/RideService.java
+++ ride-service/src/main/java/com/rideshare/rideservice/service/RideService.java
@@ -1,6 +1,9 @@
 package com.rideshare.rideservice.service;
 
+import com.rideshare.rideservice.dto.RideRequest;
+import com.rideshare.rideservice.dto.RideResponse;
 import com.rideshare.rideservice.event.RideRequestedEvent;
+import com.rideshare.rideservice.model.Ride;
 import com.rideshare.rideservice.repository.RideRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +20,15 @@ public class RideService {
     private static final String RIDE_REQUESTED_TOPIC = "ride.requested";
 
     /**
-     * 
+     * create ride in DB with REQUESTED STATUS
      */
+
+    public RideResponse requesRide(RideRequest request){
+        log.info("New ride request from rider: {}",request.getRiderID());
+
+        //Step 1: save ride to database
+
+        Ride ride = new Ride();
+        ride.setRiderId();
+    }
 }
