4da00864b09b20aba71bb119f01b13d2e9f101cc
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-04-26 11:23:41 +0200

    started with the globaleexceptionhandler

--- ride-service/src/main/java/com/rideshare/rideservice/controller/GlobalExceptionHandler.java
+++ ride-service/src/main/java/com/rideshare/rideservice/controller/GlobalExceptionHandler.java
@@ -1,4 +1,20 @@
 package com.rideshare.rideservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.method.MethodValidationException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
+    public ResponseEntity<Map<String, String>> handleValidationError(
+            MethodValidationException ex){
+
+        Map<String,String> errors = new HashMap<>();
+        
+    }
 }
