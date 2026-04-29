b5ce529583a70ef36031a700383cd106b0ebf8ed
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-04-29 09:43:20 +0200

    started with matching service

--- matching-service/src/main/java/com/rideshare/matchingservice/service/MatchingService.java
+++ matching-service/src/main/java/com/rideshare/matchingservice/service/MatchingService.java
@@ -1,4 +1,38 @@
 package com.rideshare.matchingservice.service;
 
+import com.rideshare.matchingservice.client.LocationServiceClient;
+import com.rideshare.matchingservice.dto.NearByDriverResponse;
+import com.rideshare.matchingservice.event.RideMatchedEvent;
+import com.rideshare.matchingservice.event.RideRequestedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class MatchingService {
+    private final LocationServiceClient locationServiceClient;
+    private final KafkaTemplate<String, RideMatchedEvent> kafkaTemplate;
+
+    private static final String RIDE_MATCHED_TOPIC = "ride.matched";
+    private static final double DEFAULT_SEARCH_RADIUS_KM = 5.0;
+
+    /**
+     * Main matching algorithm
+     * Called when RideReduestedEvent is consumed from Kafka
+     * @param event
+     *
+     * Steps:
+     * 1. Ask Location service for nearby Drivers
+     */
+
+    public void matchDriverForRide(RideRequestedEvent event){
+
+        List<NearByDriverResponse>
+
+    }
 }
